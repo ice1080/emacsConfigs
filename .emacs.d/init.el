412e93a64d1653136dbbc511d739b55f0a0306b2
Isaac Hudson <ice1080@gmail.com>
2019-06-22 14:42:36 -0600

    adding auto-complete and fixing init file location

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -4,6 +4,7 @@
 (add-to-list 'package-archives '("melpa" . "http://melpa.org/packages/"))
 (package-initialize)
 
+(setq user-init-file "~/src/emacsConfigs/.emacs.d/init.el")
 (setq user-emacs-directory "~/src/emacsConfigs/.emacs.d/")
 (setq package-user-dir "~/src/emacsConfigs/.emacs.d/elpa")
 
@@ -32,8 +33,9 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  '(custom-enabled-themes (quote (tsdh-dark)))
- '(package-selected-packages (quote (rjsx-mode langtool magit-popup kubernetes restclient groovy-mode exec-path-from-shell)))
-)
+ '(package-selected-packages
+   (quote
+    (auto-complete rjsx-mode langtool magit-popup kubernetes restclient groovy-mode exec-path-from-shell))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
