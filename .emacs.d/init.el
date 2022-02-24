0799bf93281e5670a1464d1c31d932c2a6ba48b1
Isaac Hudson <ice1080@gmail.com>
2022-02-23 19:35:37 -0800

    adding rust mode to emacs

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -27,7 +27,7 @@
  '(org-archive-default-command (quote org-archive-subtree))
  '(package-selected-packages
    (quote
-    (org-bullets neotree projectile auto-complete rjsx-mode langtool magit-popup kubernetes restclient groovy-mode))))
+    (rust-mode org-bullets neotree projectile auto-complete rjsx-mode langtool magit-popup kubernetes restclient groovy-mode))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
