cea53f184c478c6499ba1d7e18edc7b723429336
Isaac Hudson <ice1080@gmail.com>
2022-04-02 19:03:57 -0700

    finishing init rewrite

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -13,11 +13,14 @@
  confirm-kill-emacs 'y-or-n-p
  visible-bell t
 ;  use-package-always-ensure t ;; todo this needs testing
- inhibit-startup-screen t)
+ inhibit-startup-screen t
+ global-auto-revert-non-file-buffers t)
 
 (setq-default indent-tabs-mode nil
               tab-width 4
 	          display-line-numbers t)
+
+(global-auto-revert-mode 1) ;; revert(reload) buffers when the underlying file has changed
 
 (put 'upcase-region 'disabled nil)
 (put 'downcase-region 'disabled nil)
@@ -34,11 +37,6 @@
 
 ;; location of personal elisp lib dir, for downloaded lisp files
 (add-to-list 'load-path "~/src/emacsConfigs/lisp")
-
-;; load the local packages
-(require 'sqlplus)
-;; (use-package sqlplus ;; test this
-;;   :ensure nil)
 
 (use-package which-key
   :init
@@ -68,7 +66,6 @@
   :config
   (setq ivy-initial-inputs-alist nil))
 
-;; org mode
 (use-package org
   :hook
   (org-mode . org-bullets-mode)
@@ -76,80 +73,71 @@
   (org-log-done 'time)
   (org-ellipsis " ▾"))
 
-;; projectile settings
-(require 'subr-x) ;; necessary because of this issue: https://github.com/bbatsov/projectile/issues/1382
-(projectile-mode +1)
-(define-key projectile-mode-map (kbd "C-c C-p") 'projectile-command-map)
-(setq projectile-project-search-path '("~/src"))
-(add-to-list 'projectile-globally-ignored-directories "node_modules")
-(projectile-discover-projects-in-search-path)
-(setq projectile-completion-system 'ivy)
+(use-package projectile
+  :init
+  (projectile-mode +1)
+  :bind (:map projectile-mode-map
+              ("C-c C-p" . projectile-command-map))
+  :requires subr-x
+  :config
+  (add-to-list 'projectile-globally-ignored-directories "node_modules")
+  (projectile-discover-projects-in-search-path)
+  :custom
+  (projectile-project-search-path '("~/src"))
+  (projectile-completion-system 'ivy))
 
-;; neotree configs
-(global-set-key [f8] 'neotree-toggle)
+(use-package neotree
+  :bind ([f8] . neotree-toggle))
 
-;; js settings, ie enable jsx mode for all js files
-(add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
-(setq web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'")))
-(setq js-indent-level 4)
+(use-package web-mode
+  :init
+  (add-to-list 'auto-mode-alist '("\\.js\\'" . web-mode))
+  :custom
+  (js-indent-level 4)
+  (web-mode-content-types-alist '(("jsx" . "\\.js[x]?\\'"))))
 
-;; prettier mode based on minor modes
-(add-hook 'after-init-hook #'global-prettier-mode)
+(use-package prettier
+  :init
+  (add-hook 'after-init-hook 'global-prettier-mode))
 
 ;; flycheck configs: http://codewinds.com/blog/2015-04-02-emacs-flycheck-eslint-jsx.html
 (use-package flycheck
-  :init (global-flycheck-mode))
-;; use eslint with web-mode for jsx files
-(flycheck-add-mode 'javascript-eslint 'web-mode)
+  :init (global-flycheck-mode)
+  :config
+  ;; use eslint with web-mode for jsx files
+  (flycheck-add-mode 'javascript-eslint 'web-mode))
 
-;; auto complete
-(ac-config-default)
-;; TODO diminish the mode from mode line, or change to a different auto complete via lsp
+(use-package auto-complete
+  :diminish auto-complete-mode
+  :config
+  (ac-config-default))
 
 ;; startup commands:
 (find-file (concat user-emacs-directory "init.el"))
 
-(if (string-equal system-name "ICDT-MBPIH.local")
-    (find-file "~/OneDrive - ICD Tech/Notes/DatabaseScripts.org")
+(if (string-equal (system-name) "ICDT-MBPIH.local")
+    (progn
+      (find-file "~/OneDrive - ICD Tech/Notes/DatabaseScripts.org")
+      (find-file "~/OneDrive - ICD Tech/Notes/GeneralNotes.org")
+      (exec-path-from-shell-copy-env "DYLD_LIBRARY_PATH")
+      (exec-path-from-shell-copy-env "TNS_ADMIN"))
+  (if (string-equal (system-name) "ICDT-WKIH")
+      (progn
+        (find-file "~/OneDrive/Notes/DatabaseScripts.org")
+        (find-file "~/OneDrive/Notes/GeneralNotes.org")
+        (exec-path-from-shell-copy-env "LD_LIBRARY_PATH")
+        (exec-path-from-shell-copy-env "TNS_ADMIN"))
+    )
   )
-(if (string-equal system-name "ICDT-MBPIH.local")
-    (find-file "~/OneDrive - ICD Tech/Notes/GeneralNotes.org")
-  )
-(if (string-equal system-name "ICDT-WKIH")
-    (find-file "~/OneDrive/Notes/DatabaseScripts.org")
-  )
-(if (string-equal system-name "ICDT-WKIH")
-    (find-file "~/OneDrive/Notes/GeneralNotes.org")
-  )
-
-;; revert(reload) buffers when the underlying file has changed
-(global-auto-revert-mode 1)
-
-;; revert dired and other buffers
-(setq global-auto-revert-non-file-buffers t)
 
 ;; If linux or mac, prepare exec-path package
 (when (memq window-system '(mac ns x))
   (exec-path-from-shell-initialize))
 
-(if (eq system-type 'darwin)
-    ;; Oracle:
-    (progn
-      (exec-path-from-shell-copy-env "DYLD_LIBRARY_PATH")
-      (exec-path-from-shell-copy-env "TNS_ADMIN")
-      )
-  )
-
-(if (string-equal system-name "ICDT-WKIH")
-    ;; Oracle:
-    (progn
-      (exec-path-from-shell-copy-env "LD_LIBRARY_PATH")
-      (exec-path-from-shell-copy-env "TNS_ADMIN")
-      )
-  )
-
-;; sqlplus stuff
-(defun sqlplus-x-connect (db user pwd)
+(use-package sqlplus ;; test this
+  :ensure nil
+  :config
+  (defun sqlplus-x-connect (db user pwd)
   "Build a connection string and make a connection. The point must be in an org-mode table.
 Columns of the table must correspond to the `sqlplus-x-columns’ variable."
   (interactive)
@@ -158,4 +146,5 @@ Columns of the table must correspond to the `sqlplus-x-columns’ variable."
    (format
     "%s/%s@%s"
     user pwd db) (concat db ".sqp"))
-  )
+  ))
+
